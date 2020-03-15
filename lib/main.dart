cc247af8a62fcfd2c7d3f87a4339fb44cd3d17a2
Adam <worldelephantseal@gmail.com>
2020-03-15 14:20:18 +0000

    Modified the initial project by replacing the MyHomePage class state variable _counter with _randomInteger. Also renamed the _incrementCounter() method to _randomiseInteger() and altered its behaviour such that it updates the _randomInteger value with a random integer between 0 and 9

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 void main() => runApp(MyApp());
 
@@ -44,23 +45,25 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _randomInteger = 0;
 
-  void _incrementCounter() {
+  void _randomiseInteger() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
+      // _randomInteger without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      int randomInteger = math.Random.secure().nextInt(10);
+
+      _randomInteger = randomInteger;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
+    // by the _randomiseInteger method above.
     //
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
@@ -92,19 +95,19 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'Here is a random number:',
             ),
             Text(
-              '$_counter',
+              '$_randomInteger',
               style: Theme.of(context).textTheme.display1,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        onPressed: _randomiseInteger,
+        tooltip: 'Randomise',
+        child: Icon(Icons.autorenew),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
